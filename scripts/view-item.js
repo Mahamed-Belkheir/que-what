49b4faef18cbf0179d2cbdc8a9e45faf6479a2e8
MohamedBelkheirRBK <mohamedbel@protonmail.com>
2019-11-08 11:25:49 +0100

    set background image to be static

--- scripts/view-item.js
+++ scripts/view-item.js
@@ -3,7 +3,7 @@ var selectedPhoneId;
 function displayPhone() {
 
 	$('#phone-info').html('');
-	
+
 	var phone = db[selectedPhoneId];
 	var phoneView = $('<div class="phone-view"></div>');
 	var phoneDesc = $('<div class="phone-desc"></div>');
@@ -12,6 +12,11 @@ function displayPhone() {
 	phoneView.append(phoneDesc);
 
 	phoneDesc.append('<h3>'+phone.phoneProperty('brand')+' '+phone.phoneProperty('model')+'</h3>');
+	phoneDesc.append(`<div class='desc'><div class="name">Model: ${phone.phoneProperty('model')}</div>
+					<span class="ratings">Camera: ${phone.phoneProperty('camera')} Screen: ${phone.phoneProperty('screen')}
+					Battery: ${phone.phoneProperty('battery')} Performance: ${phone.phoneProperty('perf')}</span>
+					<div>$${phone.phoneProperty('price')}</div></div>`);
+	phoneDesc.append(`<p>${phone.phoneProperty('description')}</p>`)
 
 	$('#phone-info').append(phoneView);
 
