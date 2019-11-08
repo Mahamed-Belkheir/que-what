ee3583f8cf7b1b0fb24b8f8aed597e770f36bb42
MohamedBelkheirRBK <mohamedbel@protonmail.com>
2019-11-08 16:31:44 +0100

    finished documenting code

--- scripts/view-item.js
+++ scripts/view-item.js
@@ -1,16 +1,25 @@
+//variable to hold the ID of the selected phone to view
 var selectedPhoneId;
 
+
+//a function that checks for the selected phone id and display all it's information inside the phone-info view
 function displayPhone() {
 
+	//clear the phone info div from any previous data
 	$('#phone-info').html('');
 
+	//retrieve the phone object from the array using the selectedPhoneId value
 	var phone = db[selectedPhoneId];
+
+	//create two new divs, one to hold everything, and one to hold the phone's information
 	var phoneView = $('<div class="phone-view"></div>');
 	var phoneDesc = $('<div class="phone-desc"></div>');
 
+	//append an image of the phone to the phoneView div and then append the phoneDesc div too
 	phoneView.append('<img src='+phone.phoneProperty('img')+'>');
 	phoneView.append(phoneDesc);
 
+	//start appending the phone's data into the phoneDesc div
 	phoneDesc.append('<h3>'+phone.phoneProperty('brand')+' '+phone.phoneProperty('model')+'</h3>');
 	phoneDesc.append(`<div>
 					<span>Camera: ${phone.phoneProperty('camera')} Screen: ${phone.phoneProperty('screen')}
@@ -18,7 +27,9 @@ function displayPhone() {
 					<div>$${phone.phoneProperty('price')}</div></div>`);
 	phoneDesc.append(`<p>${phone.phoneProperty('description')}</p>`)
 
+	//append a video to the phoneView div that includes the phone's review link
 	phoneView.append('<iframe width="500" height="350" src="'+phone.phoneProperty('review')+'"></iframe>')
+	
+	//append the final phoneView div to the phone info view
 	$('#phone-info').append(phoneView);
-
 }
--- scripts/view-phones.js
+++ scripts/view-phones.js
@@ -1,34 +1,45 @@
 //function to list all the phones depending on user budget and 
 
 function listPhones(){
-
+	//empty the list-phones div
 	$('#list-phones').html('')
-	
-	 searchArray = db.filter(function(element, i){
-		element.id = i;
+	//filter the database for phones that fit within the user's inputted budget, and calculating the rating at the same time
+	var searchArray = db.filter(function(element, i){
+		element.id = i; //keep track of the phone's index in the actual array by storing it here
+
+		//here we calculate each phone's rating by multiplying it's feature rating by the user's priority
 		element.rating = Number(element.phoneProperty('camera')) * Number($('input[name=camera]:checked').val())
 					   + Number(element.phoneProperty('battery')) * Number($('input[name=battery]:checked').val())
 					   + Number(element.phoneProperty('screen')) * Number($('input[name=screen]:checked').val())
 					   + Number(element.phoneProperty('perf')) * Number($('input[name=performance]:checked').val())
+		//if a user inputted a budget, filter by it
 		if($('[type=number]').val()!=='')
 			return element.phoneProperty('price')<=$('[type=number]').val()
+		//returning everything otherwise
 		return element
 	}).sort(function(a,b){
-		return b.rating - a.rating;
+		return b.rating - a.rating; //sort the copy array by the user's rating
 	});
 
+
+	//here we start creating the divs of each search result, looping through the array of results
+	//we create a div for each one
 	searchArray.forEach(function(element, i){
+		
+		//the div that holds the entire search object
+		var pItem = $('<div class="phone-item"></div>'); 
 
-		var pItem = $('<div class="phone-item"></div>');
-
-		pItem.append(`<img src='${element.phoneProperty('img')}'>`)
+		//we append to the pItem div the item's image, and another div that contains the item's details
+		pItem.append(`<img src='${element.phoneProperty('img')}'>`) 
 		pItem.append(`<div class='desc'><div class="name">Model: ${element.phoneProperty('model')}</div>
 					<span class="ratings">Camera: ${element.phoneProperty('camera')} Screen: ${element.phoneProperty('screen')}
 					Battery: ${element.phoneProperty('battery')} Performance: ${element.phoneProperty('perf')}</span>
 					<div>$${element.phoneProperty('price')}</div></div>`)
 
+		//here we append a span showing the ranking of each phone
 		pItem.append('<span class="ranking">#'+(i+1)+'<span>');
 		
+		//here we add a click functionality to the item, which hides the search result and goes to the phone info view
 		pItem.on('click', function(){
 			selectedPhoneId = element.id;
 			$('#list-p').slideUp(function(){
@@ -36,9 +47,8 @@ function listPhones(){
 				$('#p-info').slideDown();
 			})
 		})
-
+		//append the item to the list phones view
 		$('#list-phones').append(pItem);
 	});
 }
 
-	var searchArray
